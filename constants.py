95ea0615c1fd631f1d15d0ec28418b7ea7378567
Aviraa625 <132155526+Aviraa625@users.noreply.github.com>
2024-04-30 10:22:39 +0800

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,21 @@
+WIDTH = 600 #Дэлгэцийн өргөн
+HEIGHT = 600 #Дэлгэцийн өндөр
+
+ROWS = 3 #Мөрийн тоо
+COLS = 3 #Баганын тоо
+SQSIZE = WIDTH // COLS #Нэг тэгш өнцөгтийн хэмжээ
+
+LINE_WIDTH = 15 #
+CIRC_WIDTH = 15 #
+CROSS_WIDTH = 20 # 
+
+RADIUS = SQSIZE // 3 #Радиус
+
+OFFSET = 50 # 
+
+# --- Өнгө ---
+
+BG_COLOR = (28, 170, 156) # 
+LINE_COLOR = (23, 145, 135) # 
+CIRC_COLOR = (239, 231, 200) # 
+CROSS_COLOR = (66, 66, 66) #
